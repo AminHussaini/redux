97f8bdf7494724de776d5e62baf9d55556401440
Amin Hussain <110085196+syed-amin-hussaini@users.noreply.github.com>
2023-01-10 01:38:44 +0500

    Changes

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -44,16 +44,30 @@ const postsSlice = createSlice({
             userId,
             title,
             content,
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0
+            },
             date: new Date().toISOString()
           }
         }
-      }
+      },
+    },
+    reactionAdded(state,action){
+      const {postId,reaction} = action.payload;
+      const existingPost = state.find(post => post.id === postId)
+          if (existingPost) {
+              existingPost.reactions[reaction]++
+          } 
     }
   },
 });
 
 // Add the post 
-export const {postAdded} = postsSlice.actions;
+export const {postAdded,reactionAdded} = postsSlice.actions;
 
 // For all the post
 export const selectAllPosts = (state) => state.posts;
